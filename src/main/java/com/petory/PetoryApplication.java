f2742babcf9d57a6cf14f0d0d5dd755ac1f2d710
z-zzye <elineve119@gmail.com>
2025-06-23 16:10:13 +0900

    petory 스프링부트 프로젝트 생성

---
+++ src/main/java/com/petory/PetoryApplication.java
@@ -0,0 +1,13 @@
+package com.petory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petory/PetoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
